0a9cf88ca17ebbd7a312694f0fc0db7816e3b692
Anne Treasure <anne.m.treasure@gmail.com>
2022-03-08 15:13:06 +0200

    shiny app development

--- stakeholder_map/app.R
+++ stakeholder_map/app.R
@@ -2,7 +2,8 @@
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
 
-# ----- load libraries -----
+
+####### ------- LOAD LIBRARIES ------- #######
 library(tidyverse)
 library(googlesheets4)
 library(shiny)
@@ -13,10 +14,12 @@ library(tableHTML)
 # usecairo = T from package Cairo for better quality of figures in shiny app
 options(shiny.usecairo=T)
 
-# ----- load functions -----
-#source('functions/my_map_activ.R')
 
-# ----- authorisations -----
+####### ------- LOAD FUNCTIONS ------- #######
+source('functions/my_map_activ.R')
+
+
+####### ------- AUTHORISATIONS ------- #######
 # this works locally
 gs4_auth(email = "*@talarify.co.za", path = "~/stakeholder_map/.secret/GSHEET_ACCESS")
 
@@ -28,35 +31,54 @@ gs4_auth(email = "*@talarify.co.za", path = "~/stakeholder_map/.secret/GSHEET_AC
 #            service = "GSHEET_ACCESS",
 #            token_path = ".secret/GSHEET_ACCESS")
 
-# ----- Read in Google Sheet data -----
+
+####### ------- READ in GOOGLE SHEET DATA ------- #######
 ss = "https://docs.google.com/spreadsheets/d/1dflPxRV-JEoOinBnf24hIm1tqSDCjtwLaGZWSR0VipA/edit#gid=369134759"
 
 kumu <- read_sheet(ss, sheet = "kumu") # don't need to read this sheet in for manipulation. Its ready for Kumu. Need to start a new Kumu project, and link this spreadsheet to it
 
 locations <- read_sheet(ss, sheet = "Organisation_locations")
-project <- read_sheet(ss, sheet = "project")
-person <- read_sheet(ss, sheet = "person")
+
+project <- read_sheet(ss, sheet = "project") %>%
+    merge(locations, by = 'Organisation')
+person <- read_sheet(ss, sheet = "person")%>%
+    merge(locations, by = 'Organisation')
 dataset <- read_sheet(ss, sheet = "dataset")
 tool <- read_sheet(ss, sheet = "tool")
 publication <- read_sheet(ss, sheet = "publication")
-training <- read_sheet(ss, sheet = "training")
+training <- read_sheet(ss, sheet = "training")%>%
+    merge(locations, by = 'Organisation')
 archives <- read_sheet(ss, sheet = "archives")
 learning_material <- read_sheet(ss, sheet = "learning_material")
 unclassified <- read_sheet(ss, sheet = "unclassified")
 
+####### ------- CHOICES for ACTIVITIES MAP ------- #######
+choices_record_type <- c('Person', 'Project', 'Training')
 
-#### ----- Define UI for application that draws plot -----
+
+####### ------- Define UI for application that draws plot ------- #######
 ui <- (fluidPage(
     titlePanel(title = "Digital Humanities and Computational Social Sciences landscape in South Africa"),
     p("The South African Centre for Digital Language Resources (SADiLaR) is a national centre supported by the Department of Science and Innovation (DSI) as part of the South African Research Infrastructure Roadmap (SARIR). SADiLaR has an enabling function, with a focus on all official languages of South Africa, supporting research and development in the domains of language technologies and language-related studies in the humanities and social sciences. Furthermore the centre has a mandate to develop digital humanities capacity in South Africa."),
     tabsetPanel(
         # ----- input for 1st panel (activities map) -----
         tabPanel('Activities Map',
+                 sidebarPanel(
+                     # add filter for data by record type
+                     selectInput(inputId = 'Type', 
+                                 label = 'Choose which record type to view', 
+                                 choices = choices_record_type, 
+                                 selected = 'People'
+                     ), # end selectInput
+                     # add reset button for map view
+                     actionButton("reset_button", "Reset map view")
+                 ), # end sidebarPanel
                      mainPanel(
-                         #            h4('DH and CSS landscape in South Africa'),
+                         #h4('DH and CSS landscape in South Africa'),
                          p("This map shows data on Digital Humanities (DH) and Computational Social Sciences (CSS) activities and initiatives in South Africa."),
-                         h4('Add some text here'),
-                         p("if we want text here"),
+                         p('Records from locations close to each other are grouped together.
+                    To ungroup, click on a green circle and see individual locations, click again to see individual beneficiaries at one location. Click on', strong(em('Reset map view')), 'to get back to the orginal view.'),
+                         leafletOutput("my_map_activities"),
                      ) # end mainPanel
         ), # end tabPanel 1
         
@@ -106,56 +128,67 @@ ui <- (fluidPage(
         ), # end tabPanel 10
         
     ) # end tabsetPanel
-) # end fluidpage
-)
+)) # end fluidpage
 
 # Define server logic required to draw a the leaflet map, other plots, and show tables
 server <- function(input, output){
-    # ----- output (map) for 1st panel (map) ----- ## TO COME, table for now
-    output$mymap = DT::renderDataTable({
-        locations
-    }) # end output map
+    # ----- output (map) for 1st panel (map) ----- 
+
+    output$my_map_activities <- leaflet::renderLeaflet({
+        # filter for type 
+        if(input$Type == 'Project') {
+            mapData <- project
+        }else if(input$Type == 'Person') {
+            mapData <- person
+        }else if(input$Type == 'Training'){
+            mapData <- training
+        } # end if else
+        
+        # draw the map using function
+        my_map_activ(x = mapData)
+    }) # end my_map_activities map
+    
+    # set reset button for activities map
+    observe({
+        input$reset_button
+        leafletProxy("my_map_activities") %>% 
+            setView(lng = 24.774610,  
+                    lat = -29.038968, 
+                    zoom = 5)
+    })
     
     # ----- output (table) for 2nd panel (projects table) -----
     output$mytable_project = DT::renderDataTable({
         project
     })
-    
     # ----- output (table) for 3nd panel (people table) -----
     output$mytable_person = DT::renderDataTable({
         person
     })
-    
     # ----- output (table) for 4th panel (dataset table) -----
     output$mytable_dataset = DT::renderDataTable({
         dataset
     })    
-    
     # ----- output (table) for 5th panel (tool table) -----
     output$mytable_tool = DT::renderDataTable({
         tool
     })
-    
     # ----- output (table) for 6th panel (publication table) -----
     output$mytable_publication = DT::renderDataTable({
         publication
     })
-    
     # ----- output (table) for 7th panel (training table) -----
     output$mytable_training = DT::renderDataTable({
         training
     })
-    
     # ----- output (table) for 8th panel (learning_material table) -----
     output$mytable_learning_material = DT::renderDataTable({
         learning_material
     })
-    
     # ----- output (table) for 9th panel (archives table) -----
     output$mytable_archives = DT::renderDataTable({
         archives
     })
-    
     # ----- output (table) for 10th panel (unclassified table) -----
     output$mytable_unclassified = DT::renderDataTable({
         unclassified
